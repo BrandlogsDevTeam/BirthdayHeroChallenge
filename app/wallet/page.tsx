605605565430d19e30f2209b3b834969f879a8b5
CharlieAlbert <charlieraph36@gmail.com>
2025-01-09 00:22:28 +0300

    dynamic page displays for sessions

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -1,5 +1,6 @@
 import { Layout } from "@/app/components/Layout";
 import { createClient } from "@/lib/supabase/server";
+import { WelcomeButton } from "../components/welcom-button";
 
 const Wallet = async () => {
   const supabase = await createClient();
@@ -8,9 +9,14 @@ const Wallet = async () => {
   } = await supabase.auth.getUser();
   return (
     <Layout>
-      <h1 className="text-4xl font-bold mb-4">Wallet</h1>
-      <p className="text-lg">Content goes here.</p>
-      {user && <p className="text-lg">Welcome, {user.email}!</p>}
+      {user ? (
+        <div>
+          <h1 className="text-4xl font-bold mb-4">Wallet</h1>
+          <p className="text-lg">Content goes here.</p>
+        </div>
+      ) : (
+        <WelcomeButton currentPage="wallet" />
+      )}
     </Layout>
   );
 };
