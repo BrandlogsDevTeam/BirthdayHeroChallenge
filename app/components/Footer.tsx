d59624093b3862b18ba96d80a3312f18df3a4811
CharlieAlbert <charlieraph36@gmail.com>
2025-01-11 10:33:09 +0300

    fix: footer visibility

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,10 @@
 export function Footer() {
   return (
-    <footer className="border-t">
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+    <footer className="border-t w-full bg-gray-100">
+      <div className="container flex flex-col items-center justify-center md:h-20 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <p className="text-left text-sm leading-loose sm:text-center">
-            © 2024 Brandlogs Inc. All rights reserved.
+          <p className="text-left leading-loose sm:text-center">
+            © {new Date().getFullYear()} Brandlogs Inc. All rights reserved.
           </p>
         </div>
       </div>
