45549c02c1521dc3f1c3d1de94f801ac06710e70
CharlieAlbert <charlieraph36@gmail.com>
2025-01-17 19:13:19 +0300

    solved merge conflict

--- lib/supabase/server-extended/brandProfile.ts
+++ lib/supabase/server-extended/brandProfile.ts
@@ -55,20 +55,21 @@ export const endorseBrand = async (brand_profile: Partial<BrandProfile>) => {
     const { data: ls, error } = await supabase
       .schema("bhc")
       .from("log_stories")
-      .insert([{
-        ...default_content,
-        description: validData.endorsement_message || default_content.description,
-        start_date: new Date('01-01-2025').toISOString(),
-        end_date: new Date('12-31-2029').toISOString(),
-        start_time: "00:00",
-        end_time: "23:59",
-        brand_origin: data[0].id,
-        is_brand_origin: true
-      }]);
-  })()
+      .insert([
+        {
+          ...default_content,
+          description:
+            validData.endorsement_message || default_content.description,
+          start_date: new Date("01-01-2025").toISOString(),
+          end_date: new Date("12-31-2029").toISOString(),
+          start_time: "00:00",
+          end_time: "23:59",
+          brand_origin: data[0].id,
+          is_brand_origin: true,
+        },
+      ]);
+  })();
 
-  return { data }
-}
   return { data };
 };
 
