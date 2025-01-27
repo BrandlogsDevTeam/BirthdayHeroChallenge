262f6f4d6073c6c538e474ef3462ccebd7b3d83d
CharlieAlbert <charlieraph36@gmail.com>
2025-01-27 14:14:20 +0300

    fix: build error

--- app/user-profile/[username]/page.tsx
+++ app/user-profile/[username]/page.tsx
@@ -1,12 +1,6 @@
 "use server";
-
-import { fetchSession } from "@/lib/supabase/server";
-import { Layout } from "@/app/components/Layout";
 import ProfileSection from "../profile";
 
 export default async function ProfilePage({ params }: { params?: any }) {
-
-  const username = (await params)?.username;
-
-  return <ProfileSection username={username} />;
+  return <ProfileSection username={(await params)?.username} />;
 }
