48aad484e02e728d3defca0a509038176bca0410
CharlieAlbert <charlieraph36@gmail.com>
2025-02-07 11:19:37 +0300

    font optimization

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,8 +1,15 @@
-import { Open_Sans } from "next/font/google";
+import { Open_Sans, Montserrat } from "next/font/google";
 
 export const openSans = Open_Sans({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-open-sans",
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-montserrat",
   weight: ["300", "400", "500", "600", "700"],
 });
