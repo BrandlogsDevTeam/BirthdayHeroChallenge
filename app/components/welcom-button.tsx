ce694ec8dbdd82b408d3a6fa254af3995e3888a3
CharlieAlbert <charlieraph36@gmail.com>
2025-02-22 16:46:08 +0300

    public view fix

--- app/components/welcom-button.tsx
+++ app/components/welcom-button.tsx
@@ -9,9 +9,12 @@ export function WelcomeButton({ currentPage }: { currentPage: string }) {
 
   return (
     <div className="flex flex-col items-center justify-center gap-6 p-8 rounded-lg">
-      <h2 className="text-2xl font-bold text-center mb-4">
-        Birthday Hero Community
-      </h2>
+      <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-gray-700 mb-3 md:mb-4">
+        BIRTHDAY HERO CHALLENGE
+      </h1>
+      <h3 className="text-lg md:text-xl lg:text-2xl text-gray-500 mb-4 md:mb-6 px-4 md:px-8">
+        Rewards you $250 Gift Bonus to ensure no child goes to bed hungry.
+      </h3>
       <div className="flex flex-col items-center">
         It's free!
         <AcceptNomination />
