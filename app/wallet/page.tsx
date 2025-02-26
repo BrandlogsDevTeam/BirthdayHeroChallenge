52cf05bdb4b7dc30f920f3ffedb50c3aadc70216
CharlieAlbert <charlieraph36@gmail.com>
2025-02-26 09:28:09 +0300

    update wallet page: adjust current balance and remove cake giveaway

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -31,7 +31,7 @@ const WalletInterface = () => {
       <div className="flex justify-between items-center">
         <div className="flex-1 text-center">
           <h2 className="text-lg font-semibold text-gray-600">Current</h2>
-          <p className="text-4xl font-bold text-green-600">$300.00</p>
+          <p className="text-4xl font-bold text-green-600">$250.00</p>
         </div>
         <div className="border-l border-gray-300 h-16"></div>
         <div className="flex-1 text-center">
@@ -104,28 +104,28 @@ const WalletBreakdown = () => {
       available: "$0.00",
       iconColor: "text-pink-500",
     },
-    {
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 text-yellow-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9 3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z"
-          />
-        </svg>
-      ),
-      title: "Cake Giveaway",
-      current: "$50.00",
-      available: "$0.00",
-      iconColor: "text-yellow-500",
-    },
+    // {
+    //   icon: (
+    //     <svg
+    //       xmlns="http://www.w3.org/2000/svg"
+    //       className="h-6 w-6 text-yellow-500"
+    //       fill="none"
+    //       viewBox="0 0 24 24"
+    //       stroke="currentColor"
+    //     >
+    //       <path
+    //         strokeLinecap="round"
+    //         strokeLinejoin="round"
+    //         strokeWidth="2"
+    //         d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9 3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z"
+    //       />
+    //     </svg>
+    //   ),
+    //   title: "Cake Giveaway",
+    //   current: "$50.00",
+    //   available: "$0.00",
+    //   iconColor: "text-yellow-500",
+    // },
     {
       icon: (
         <svg
